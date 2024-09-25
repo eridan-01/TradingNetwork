f8fc73a74dda1b32e44a29475586986a199f1790
Eugene Geym <geymzheka@gmail.com>
2024-09-25 23:33:23 +0500

    added: admin action, admin register, link

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import NetworkNode
+
+
+@admin.action(description='Очистка задолженности')
+def clear_debt(queryset):
+    queryset.update(debt=0)
+
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_filter = ['city']
+    list_display = ['name', 'country', 'city', 'supplier_link', 'debt']
+    actions = [clear_debt]
+
+    def supplier_link(self, obj):
+        if obj.supplier:
+            link = reverse('admin:network_networknode_change', args=[obj.supplier.id])
+            return format_html('<a href="{}">{}</a>', link, obj.supplier.name)
+        return '-'
+    supplier_link.short_description = 'Поставщик'
